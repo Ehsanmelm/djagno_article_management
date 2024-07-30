836337a92d798dd502f53f3208581704298c3635
Ehsan <ehsanmelm82@gmail.com>
2024-07-30 14:39:21 +0330

    feat : create article model

--- article/models.py
+++ article/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+
+class ArticleModel(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    author= models.ForeignKey(settings.AUTH_USER_MODEL , on_delete=models.CASCADE)
